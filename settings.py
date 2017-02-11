54c4fc7a1e39439a1bc3ff197f58ae527e5f2775
taohongxiu <290191473@qq.com>
2017-02-11 13:00:14 +0800

    delete alert

--- settings.py
+++ settings.py
@@ -3,3 +3,10 @@ INFLUX_PORT = 8086
 INFLUX_USER = None
 INFLUX_PASSWORD = None
 INFLUX_DB = "datadog"
+
+SMTP_SMARTHOST = 'localhost:25'
+SMTP_FROM = 'alertmanager@example.org'
+SMTP_AUTH_USERNAME = 'alertmanager'
+SMTP_AUTH_PASSWORD = 'password'
+
+
